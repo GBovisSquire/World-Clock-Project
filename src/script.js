9e23355e6e97e2841d8858f8d572dfa735646e41
GBovisSquire <gracejbsquire@gmail.com>
2024-08-21 18:59:43 +0100

    added current location

--- src/script.js
+++ src/script.js
@@ -29,6 +29,11 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current");
+  {
+    cityTimezone = moment.tz.guess();
+  }
+
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let citiesContainerElement = document.querySelector("#citiesContainer");
